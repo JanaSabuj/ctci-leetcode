2abb5e9f9964e40b8443fcea5e1e4aad7f597ec2
Sabuj Jana <sabujlovescoding@gmail.com>
2021-02-25 18:45:12 +0530

    Update 98. Validate Binary Search Tree.cpp

--- Ch 4 - Trees and Graphs/98. Validate Binary Search Tree.cpp
+++ Ch 4 - Trees and Graphs/98. Validate Binary Search Tree.cpp
@@ -35,3 +35,24 @@ public:
         return true;
     }
 };
+-----------------------------------------
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+typedef long long ll;
+class Solution {
+public:
+    bool isValidBST(TreeNode* root, ll minval = LLONG_MIN, ll maxval = LLONG_MAX) {
+        if(root == NULL)
+            return true;
+        return minval < root->val and root->val < maxval and isValidBST(root->left, minval, root->val) and isValidBST(root->right, root->val, maxval);
+    }
+};
